16c32e4fad3037a2e4eea276782a68c86d04da83
Ehsanul Hoque <gamerrishad@gmail.com>
2017-12-18 19:00:21 +0600

    Test Upload

--- app/src/main/java/com/eatit/user/eatit/Home.java
+++ app/src/main/java/com/eatit/user/eatit/Home.java
@@ -3,20 +3,19 @@ package com.eatit.user.eatit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.eatit.user.eatit.Common.Common;
 import com.eatit.user.eatit.Interface.ItemClickListener;
@@ -143,7 +142,6 @@ public class Home extends AppCompatActivity
 
         }
 
-        // Testing Second Commit &&
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
